0413ad0819df5a823b2f3c1ab66267557b57f770
Luanbarradas <barradas.luk@gmail.com>
2024-07-25 18:15:35 -0300

    feat(form): refactor form and create calculation function

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,60 +1,71 @@
 import React from "react";
-import { polos, Polo } from "../db/Database";
+import { polos } from "../db/Database";
 
 export const Form: React.FC = () => {
-  const [position, setPosition] = React.useState<number | "">("");
-  const [currentPole, setCurrentPole] = React.useState<string>("");
-  const [targetPole, setTargetPole] = React.useState<string>("");
+  const [rank, setRank] = React.useState<number | "">("");
+  const [currentPole, setCurrentPole] = React.useState<number>(0);
+  const [targetPole, setTargetPole] = React.useState<number>(0);
+  const [result, setResult] = React.useState<number | null>(null);
 
-  const handlePositionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPosition(Number(e.target.value));
-  };
+  const handleCalculate = () => {
+    const myPole = polos.find((polo) => polo.id === currentPole);
+    const destinationPole = polos.find((polo) => polo.id === targetPole);
 
-  const handleCurrentPoleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setCurrentPole(e.target.value);
-  };
+    if (myPole && destinationPole && typeof rank === "number") {
+      const placementPercentage = ((rank - 1) / myPole.vacancies) * 100;
+      const newPosition =
+        1 + (placementPercentage * destinationPole.vacancies) / 100;
+      const roundedPosition = Math.round(newPosition);
+      setResult(roundedPosition);
 
-  const handleTargetPoleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setTargetPole(e.target.value);
+      setCurrentPole(0);
+      setTargetPole(0);
+    }
   };
 
   return (
-    <form>
-      <label htmlFor="position">Posição</label>
-      <input
-        type="number"
-        id="position"
-        min="1"
-        value={position}
-        onChange={handlePositionChange}
-        required
-      />
-      <label htmlFor="currentPole">Escolha o polo atual:</label>
-      <select
-        id="currentPole"
-        value={currentPole}
-        onChange={handleCurrentPoleChange}
-      >
-        <option value="">Selecione um polo</option>
-        {polos.map((polo: Polo) => (
-          <option key={polo.value} value={polo.value}>
-            {polo.value}
-          </option>
-        ))}
-      </select>
-      <label htmlFor="targetPole">Escolha o polo de destino:</label>
-      <select
-        id="targetPole"
-        value={targetPole}
-        onChange={handleTargetPoleChange}
-      >
-        <option value="">Selecione um polo</option>
-        {polos.map((polo: Polo) => (
-          <option key={polo.value} value={polo.value}>
-            {polo.value}
-          </option>
-        ))}
-      </select>
-    </form>
+    <div className="styled-form">
+      <label className="styled-label">
+        Ranque:
+        <input
+          className="styled-input"
+          type="number"
+          value={rank}
+          onChange={(e) => setRank(Number(e.target.value))}
+        />
+      </label>
+      <label className="styled-label">
+        Meu Polo:
+        <select
+          className="styled-input"
+          value={currentPole}
+          onChange={(e) => setCurrentPole(Number(e.target.value))}
+        >
+          <option value={0}>Selecione o Polo Atual</option>
+          {polos.map((polo) => (
+            <option key={polo.id} value={polo.id}>
+              {polo.local}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label className="styled-label">
+        Outros Polos:
+        <select
+          className="styled-input"
+          value={targetPole}
+          onChange={(e) => setTargetPole(Number(e.target.value))}
+        >
+          <option value={0}>Selecione o Polo de Destino</option>
+          {polos.map((polo) => (
+            <option key={polo.id} value={polo.id}>
+              {polo.local}
+            </option>
+          ))}
+        </select>
+      </label>
+      <button onClick={handleCalculate}>Calcular</button>
+      {result !== null && <p>Nova Posição: {result}</p>}
+    </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./css/index.css";
+import "./css/global.css";
+import "./css/form.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
